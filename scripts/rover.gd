88d8861e1232efa0217c9cf9772a2317e256277c
Oliver Gilliland <omgilliland@gmail.com>
2021-10-17 09:36:02 +0100

    Rover movement test

--- scripts/rover.gd
+++ scripts/rover.gd
@@ -1,4 +1,21 @@
 extends KinematicBody
 
-func _physics_process(delta) -> void:
-	pass
+const speed : int = 1
+const rot_speed : float = 0.05
+const friction : float = 0.95
+const gravity : int = 10
+
+var velocity := Vector3()
+
+func _physics_process(delta : float) -> void:
+	var direction := Vector2(
+		Input.get_action_strength("move_down") - Input.get_action_strength("move_up"),
+		Input.get_action_strength("move_left") - Input.get_action_strength("move_right")
+	).normalized()
+	
+	velocity += direction.x * speed * global_transform.basis.z
+	rotate_y(direction.y * rot_speed)
+	velocity *= friction
+	velocity -= gravity * global_transform.basis.y
+	
+	velocity = move_and_slide_with_snap(velocity, -get_floor_normal(), global_transform.basis.y, true)
