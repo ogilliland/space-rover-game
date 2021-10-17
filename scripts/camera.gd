f7abf1147f46c0aff92ae3bc533d2801c26ff1d7
Oliver Gilliland <omgilliland@gmail.com>
2021-10-17 17:27:48 +0100

    Improved radar countouring

--- scripts/camera.gd
+++ scripts/camera.gd
@@ -1,7 +1,11 @@
 extends Camera
 
 export var target : NodePath
+export var copy_rotation : bool = true
 
 func _physics_process(_delta : float) -> void:
 	if target:
-		global_transform = get_node(target).global_transform
+		if copy_rotation:
+			global_transform = get_node(target).global_transform
+		else:
+			global_transform.origin = get_node(target).global_transform.origin
